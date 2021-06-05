cb12592b77b77de737aeaaab0c7f9c6d4f849f3e
Ethan Wang <ethan_wang@berkeley.edu>
2021-06-04 19:15:42 -0700

    pygameerror

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import math
 from queue import PriorityQueue
 
 WIDTH = 800
-WIN  = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("A* Path Finding Algorithm")
+WIN  = pygame.display.set_mode((WIDTH, WIDTH)) # setting the display size of the pygame
+pygame.display.set_caption("A* Path Finding Algorithm") # creating a caption
